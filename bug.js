67d10f9ce1364644246a91fdf43c15dca654d65d
BugHunter-X <askzenai@gmail.com>
2025-01-15 20:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase Realtime Database client sometimes throws an error 'Failed to get auth token' even when the authentication is correctly set up and the user is logged in.  This happens intermittently, making it difficult to pinpoint the cause. The error doesn't provide much detail, hindering debugging efforts.
